424731c692829c210a54d713a7a0e0a71c933040
adiallo792 <56573046+adiallo792@users.noreply.github.com>
2020-01-19 20:14:08 -0800

    candrive file added

---
+++ CanDrive.js
@@ -0,0 +1,7 @@
+var FirstName = "Aissatou";
+var LastName = "Diallo";
+var Age = 23;
+var drivingAge = 16;
+if (Age >= drivingAge){
+  console.log( FirstName, LastName, "Can drive and has been driven for", Age - drivingAge, );
+}
