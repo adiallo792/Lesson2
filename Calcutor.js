56e05c757ed6f644af494eefd3cfeb9bd9f955d3
adiallo792 <56573046+adiallo792@users.noreply.github.com>
2020-01-19 16:24:48 -0800

    My First Javascript file

---
+++ Calcutor.js
@@ -0,0 +1,8 @@
+let x = 10;
+let y = 42;
+let z = 23;
+let w = 34;
+console.log("multiplication: " + x * z);
+console.log("addition: ", x + y);
+console.log("division: ", z / w);
+console.log("sustraction: ", w - y);
